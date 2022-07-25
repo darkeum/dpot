294de888a2363f3c2e43a8db93cb4dec1e5bf0e2
darkeum <vaneka97@ya.ru>
2022-07-25 17:00:43 +0400

    Первая публикация

---
+++ src/Console/PublishCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Darkeum\Dpot\Console;
+
+use Boot\System\Console\Command;
+
+class PublishCommand extends Command
+{
+    /**
+     * Имя и параметры консольной команды
+     *
+     * @var string
+     */
+    protected $signature = 'dpot:publish';
+
+    /**
+     * Описание консольной команды
+     *
+     * @var string
+     */
+    protected $description = 'Публикация Docker файла для Darklyy';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->call('vendor:publish', ['--tag' => 'dpot-docker']);
+
+        file_put_contents(
+            $this->laravel->basePath('docker-compose.yml'),
+            str_replace(
+                [
+                    './vendor/darkeum/dpot/runtimes/8.1',
+                ],
+                [
+                    './docker/8.1',
+                ],
+                file_get_contents($this->laravel->basePath('docker-compose.yml'))
+            )
+        );
+    }
+}
---
+++ src/SailServiceProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Darkeum\Dpot\Sail;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Boot\Support\ServiceProvider;
+use Darkeum\Dpot\Console\InstallCommand;
+use Darkeum\Dpot\Console\PublishCommand;
+
+class SailServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Запуск сервис провайдера
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerCommands();
+        $this->configurePublishing();
+    }
+
+    /**
+     * Регистрация команд
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallCommand::class,
+                PublishCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Настройка публикации для пакета.
+     *
+     * @return void
+     */
+    protected function configurePublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../runtimes' => $this->app->basePath('docker'),
+            ], ['sail', 'sail-docker']);
+
+            $this->publishes([
+                __DIR__ . '/../bin/sail' => $this->app->basePath('sail'),
+            ], ['sail', 'sail-bin']);
+        }
+    }
+
+    /**
+     * Получение услуг, предоставляемые провайдером.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            InstallCommand::class,
+            PublishCommand::class,
+        ];
+    }
+}
