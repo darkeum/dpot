02918394aa53d1fa57485967323e26b7437b3988
darkeum <vaneka97@ya.ru>
2022-07-26 15:35:32 +0400

    Исправления

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -76,41 +76,41 @@ class InstallCommand extends Command
      */
     protected function buildDockerCompose(array $services)
     {
-        $depends = collect($services)
-            ->filter(function ($service) {
-                return in_array($service, ['mysql', 'pgsql', 'mariadb', 'redis', 'meilisearch', 'minio', 'selenium']);
-            })->map(function ($service) {
-                return "            - {$service}";
-            })->whenNotEmpty(function ($collection) {
-                return $collection->prepend('depends_on:');
-            })->implode("\n");
+        // $depends = collect($services)
+        //     ->filter(function ($service) {
+        //         return in_array($service, ['mysql', 'pgsql', 'mariadb', 'redis', 'meilisearch', 'minio', 'selenium']);
+        //     })->map(function ($service) {
+        //         return "            - {$service}";
+        //     })->whenNotEmpty(function ($collection) {
+        //         return $collection->prepend('depends_on:');
+        //     })->implode("\n");
 
-        $stubs = rtrim(collect($services)->map(function ($service) {
-            return file_get_contents(__DIR__ . "/../../stubs/{$service}.stub");
-        })->implode(''));
+        // $stubs = rtrim(collect($services)->map(function ($service) {
+        //     return file_get_contents(__DIR__ . "/../../stubs/{$service}.stub");
+        // })->implode(''));
 
-        $volumes = collect($services)
-            ->filter(function ($service) {
-                return in_array($service, ['mysql', 'pgsql', 'mariadb', 'redis', 'meilisearch', 'minio']);
-            })->map(function ($service) {
-                return "    sail-{$service}:\n        driver: local";
-            })->whenNotEmpty(function ($collection) {
-                return $collection->prepend('volumes:');
-            })->implode("\n");
+        // $volumes = collect($services)
+        //     ->filter(function ($service) {
+        //         return in_array($service, ['mysql', 'pgsql', 'mariadb', 'redis', 'meilisearch', 'minio']);
+        //     })->map(function ($service) {
+        //         return "    sail-{$service}:\n        driver: local";
+        //     })->whenNotEmpty(function ($collection) {
+        //         return $collection->prepend('volumes:');
+        //     })->implode("\n");
 
         $dockerCompose = file_get_contents(__DIR__ . '/../../stubs/docker-compose.stub');
 
-        $dockerCompose = str_replace('{{depends}}', empty($depends) ? '' : '        '.$depends, $dockerCompose);
-        $dockerCompose = str_replace('{{services}}', $stubs, $dockerCompose);
-        $dockerCompose = str_replace('{{volumes}}', $volumes, $dockerCompose);
+        // $dockerCompose = str_replace('{{depends}}', empty($depends) ? '' : '        '.$depends, $dockerCompose);
+        // $dockerCompose = str_replace('{{services}}', $stubs, $dockerCompose);
+        // $dockerCompose = str_replace('{{volumes}}', $volumes, $dockerCompose);
 
-        // Replace Selenium with ARM base container on Apple Silicon...
-        if (in_array('selenium', $services) && php_uname('m') === 'arm64') {
-            $dockerCompose = str_replace('selenium/standalone-chrome', 'seleniarm/standalone-chromium', $dockerCompose);
-        }
+        // // Replace Selenium with ARM base container on Apple Silicon...
+        // if (in_array('selenium', $services) && php_uname('m') === 'arm64') {
+        //     $dockerCompose = str_replace('selenium/standalone-chrome', 'seleniarm/standalone-chromium', $dockerCompose);
+        // }
 
-        // Remove empty lines...
-        $dockerCompose = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $dockerCompose);
+        // // Remove empty lines...
+        // $dockerCompose = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $dockerCompose);
 
         file_put_contents($this->laravel->basePath('docker-compose.yml'), $dockerCompose);
     }
